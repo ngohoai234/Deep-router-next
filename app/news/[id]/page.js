414fd300820923335dc2258d1712d680fde7fc48
ngohoai234 <cangod05@gmail.com>
2025-02-11 20:34:50 +0700

    chore: add new pages

--- app/news/[id]/page.js
+++ app/news/[id]/page.js
@@ -1,11 +1,16 @@
+import { DUMMY_NEWS } from '@/dummy-news';
+
 const NewsDetailPage = ({ params }) => {
   const newsId = params.id;
-
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsId);
   return (
-    <>
-      <h1>News Detail Page</h1>
-      <p>News ID: {newsId}</p>
-    </>
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+      </header>
+      <h1>{newsItem.title}</h1>
+      <time dateTime={newsItem.date}>{newsItem.date}</time>
+    </article>
   );
 };
 
