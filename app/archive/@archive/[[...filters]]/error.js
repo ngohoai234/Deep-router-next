0eb8d8f0ab15bba4e030324f9ae8f46f84c633fd
ngohoai234 <cangod05@gmail.com>
2025-02-11 22:22:08 +0700

    chore: add error page

---
+++ app/archive/@archive/[[...filters]]/error.js
@@ -0,0 +1,12 @@
+'use client';
+
+const FilterError = () => {
+  return (
+    <div id="error">
+      <h2>An error occurred</h2>
+      <p>Invalid path</p>
+    </div>
+  );
+};
+
+export default FilterError;
