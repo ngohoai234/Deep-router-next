214c6646455bb1b62348e2c5c4f414e4e9331896
ngohoai234 <cangod05@gmail.com>
2025-02-12 21:48:21 +0700

    chore: add middleware

---
+++ middleware.js
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+// This function can be marked `async` if using `await` inside
+/**
+ *
+ * @param {NextRequest} request
+ * @returns
+ */
+export function middleware(request) {
+  return NextResponse.redirect(new URL('/home', request.url));
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/about/:path*',
+};
