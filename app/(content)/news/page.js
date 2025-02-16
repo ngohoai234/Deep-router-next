a17fd3d2fa06b153c9d3a1885e72da0423a6ad53
ngohoai234 <cangod05@gmail.com>
2025-02-16 19:49:44 +0700

    chore: add backend server

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,14 @@
 import NewsList from '@/component/new-list';
-import { DUMMY_NEWS } from '@/dummy-news';
 
-export default function NewsPage() {
+export default async function NewsPage() {
+  const response = await fetch('http://localhost:8080/news');
+
+  const news = await response.json();
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </>
   );
 }
